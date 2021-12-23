f5b02c1b55e0e108c3a7a920c3cef309a5439096
Everson Batista <eversonbalcantara@gmail.com>
2021-12-22 22:14:13 -0300

    Fim do dia

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -5,16 +5,32 @@ const criarTarefa = (evento) => {
   const input = document.querySelector("[data-form-input]");
   const valor = input.value;
 
-  const tarefa = document.createElement('li')
-  tarefa.classList.add('task')
+  const tarefa = document.createElement("li");
+  tarefa.classList.add("task");
   const conteudo = `<p class="content">${valor}</p>`;
 
-  tarefa.innerHTML = conteudo
+  tarefa.innerHTML = conteudo;
 
-  lista.appendChild(tarefa)
+  tarefa.appendChild(BotaoConclui());
+  lista.appendChild(tarefa);
   input.value = " ";
 };
 
 const novaTarefa = document.querySelector("[data-form-button]");
 
 novaTarefa.addEventListener("click", criarTarefa);
+
+const BotaoConclui = () => {
+  const botaoConclui = document.createElement("button");
+
+  botaoConclui.classList.add('check-button')
+  botaoConclui.innerText = "Concluir"
+
+  botaoConclui.addEventListener("click", concluirTarefa);
+
+  return botaoConclui;
+};
+
+const concluirTarefa = (evento) => {
+    const botaoConclui = evento.target
+}
